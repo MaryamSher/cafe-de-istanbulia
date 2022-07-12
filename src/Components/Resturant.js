ca4cf9ed2de4c9f2c4154c8b95e13eb47d821735
Maryam Saleem Sher <maryam@Maryams-Air.attlocal.net>
2022-07-12 16:10:24 -0500

    footer changes

--- src/Components/Resturant.js
+++ src/Components/Resturant.js
@@ -23,10 +23,23 @@ const Resturant = () => {
       <MenuCard menuData={menuData} />
       <footer className='foot'>
         <h1>CAFE DE ISTANBULIA</h1>
+        <ul className='socials'>
+          <li> <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i></a>  </li>
+          <li> <a href="https://www.twitter.com/"> <i class="fab fa-twitter"></i></a> </li>
+          <li> <a href="https://www.pinterest.com/"> <i class="fab fa-pinterest-p"></i></a>  </li>
+          <li> <a href="https://www.instagram.com/"> <i class="fab fa-instagram"></i></a>  </li>
+        </ul>
+        
         <div className="contact-info">
           <p>Plaza No 158-A, F/F Commercial</p>
           <p>Contact : 0300-7177849</p>
         </div>
+        <div className='copyright'>
+
+          <p>Created & Designed by Maryam Sher</p>
+        
+        </div>
+        <p className='cp'>Copyright © 2022 </p>
       </footer>
     </>
   );
